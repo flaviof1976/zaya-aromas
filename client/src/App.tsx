b04a7e4d9fb4a918ae510d7179c6574c80902f93
Flávio Ferreira <158588405+flaviof1976@users.noreply.github.com>
2026-09-14 20:19:49 -0300

    Adiciona rota para Termos de Uso

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,14 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Terms from "./pages/Terms";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
       <Route path={"/politica-de-privacidade"} component={PrivacyPolicy} />
+      <Route path={"/termos-de-uso"} component={Terms} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
